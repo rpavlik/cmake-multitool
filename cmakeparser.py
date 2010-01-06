0f83afb2ac5c947994e511ee5879a07b3f281d9a
Ryan Pavlik <rpavlik@iastate.edu>
2010-01-06 11:37:54 -0600

    Add more basic tests, and get the innards of the parser going a bit

--- cmakeparser.py
+++ cmakeparser.py
@@ -8,4 +8,110 @@ http://academic.cleardefinition.com
 Iowa State University HCI Graduate Program/VRAC
 """
 
+import re
 
+class ParseInput():
+	def __init__(self, strdata):
+		self.input = strdata.splitlines()
+		self.lineindex = 0
+		self.alldone = False
+		self.gotline = False
+
+	def __iter__(self):
+		"""Be usable as an iterator."""
+		return self
+
+	def next(self):
+		"""Return the current line each time we are iterated.
+		We don't go to the next line unless we've been accepted."""
+
+		# Will always hit this condition if all works well
+		if self.lineindex == len(self.lineindex):
+			self.alldone = true
+			raise StopIteration
+
+		# If we break, break big
+		assert self.lineindex < len(self.lineindex)
+
+		# OK, we can actually return the data now
+		self.gotline = true
+		return self.input[self.lineindex]
+
+	def line(self):
+		"""A more intuitive alias for next() - get the current line"""
+		return self.next()
+
+	def accept(self):
+		"""Signal that we've processed this line and should go to the next"""
+
+		# We shouldn't accept a line we haven't even seen
+		assert self.gotline == true
+		self.lineindex = self.lineindex + 1
+		self.gotline = False
+
+class CMakeParser():
+	## A tuple containing all functions that start a block
+	blockBeginnings = ('foreach',
+					'function',
+					'if',
+					'elseif',
+					'else',
+					'macro',
+					'while')
+
+	## A compiled regex that matches exactly the functions that start a block
+	reBlockBeginnings = re.compile(r"^(" + "|".join(blockBeginnings) + r")$",
+		re.IGNORECASE)
+
+	## A dictionary where blockEndings[blockBeginFunc]=(blockEndFunc1,...),
+	## all datatypes are strings.  Size is one more than blockBeginnings
+	## because we match the empty string opening function (dummy for start
+	## of file) with a dummy EOF string as an end tag
+	blockEndings = {'' : ('_CMAKE_PARSER_EOF_FLAG_',),
+
+					'foreach'	: ('endforeach',),
+
+					'function'	: ('endfunction',),
+
+					'if'		: ('elseif', 'else', 'endif'),
+					'elseif'	: ('elseif', 'else', 'endif'),
+					'else'		: ('endif',),
+
+					'macro'	: ('endmacro',),
+
+					'while'	: ('endwhile',)}
+
+	# Catch any "developer failed to add new function to both lists" bugs
+	assert len(blockEndings) - 1 == len(blockBeginnings)
+
+	## A big list comprehension that makes a dictionary with pairs (start, end)
+	## where start is a start tag string from blockEndings and end is a
+	## compiled regex that only exactly matches any corresponding ending
+	reBlockEndings = dict([  # Make a dict from list comprehension of pairs
+		# the dictionary key is just the start tag
+		( beginning,
+		# the value is a compiled regex matching any of the end tags
+		re.compile(r"^(" + "|".join(ends) + r")$", re.IGNORECASE) )
+
+		# for every key-value pair in the non-compiled dictionary
+		for beginning, ends in blockEndings.iteritems() ])
+
+	# sanity check the comprehension above
+	assert len(blockEndings) == len(reBlockEndings)
+
+	def __init__(self, parseinput):
+		self.input = parseinput
+		self.parsetree = []
+
+	def parse(self):
+		pass
+
+	def parse_block_children(self, startTag):
+		endblock = self.reBlockEndings[startTag]
+		for line in self.input:
+			func, args, comment = self.parse_line(line)
+
+def parse_string(instr):
+	parser = CMakeParser(ParseInput(instr))
+	parser.parse()
+	return parser
--- cmakeparser_test.py
+++ cmakeparser_test.py
@@ -12,6 +12,38 @@ import unittest
 import cmakeparser
 
 ## Requirement:
+## Be able to parse a valid cmake command input line.
+class ParseCompleteLine(unittest.TestCase):
+	data = (	("",				("", "", "")),
+				("func()",			("func", "", "")),
+				("func(arg)",		("func", "arg", "")),
+				("func(arg arg)",	("func", "arg arg", "")),
+				("func() # cmnt",	("func", "", "# cmnt"))	)
+	def testParseLine(self):
+		parser = cmakeparser.CMakeParser(cmakeparser.ParseInput(""))
+		for (line, expected) in self.data:
+			actual = parser.parse_line(line)
+			self.assertEqual(actual, expected)
+
+## Requirement:
+## Be able to parse a comment line
+class ParseCommentLine(unittest.TestCase):
+	data = (	"#",
+				"# some comment going on here",
+				"## a bit confusing",
+				"#a#a#a#"	)
+
+## Requirement:
+## Notify on an incomplete cmake input line
+class ParsePartialLine(unittest.TestCase):
+	data = (	("",				("", "", "")),
+				("func()",			("func", "", "")),
+				("func(arg)",		("func", "arg", "")),
+				("func(arg arg)",	("func", "arg arg", "")),
+				("func() # cmnt",	("func", "", "# cmnt"))	)
+
+
+## Requirement:
 ## A given source file has only valid one parse
 class KnownValues(unittest.TestCase):
 	def setUp(self):
@@ -22,10 +54,26 @@ class KnownValues(unittest.TestCase):
 		parses.sort()
 		self.cases = []
 		for cmakefn, parsefn in zip(cmakes, parses):
-			
-			
+			cmakef = open(cmakefn, 'r')
+			cmakedata = cmakef.read()
+			cmakef.close()
+
+			parsef = open(parsefn, 'r')
+			parsedata = eval(parsef.read())
+			parsef.close()
+			self.cases.append( (cmakefn, cmakedata, parsedata) )
+
 	def testToKnownParses(self):
-		pass
+		for cmakefn, instring, expected in self.cases:
+			out = cmakeparser.parse_string(instring)
+			self.assertEqual(str(out.parsetree), str(expected))
+			self.assertEqual(out.parsetree, expected)
+
+		for cmakefn, instring, expected in self.cases:
+			out = cmakeparser.parse_file(cmakefn)
+			self.assertEqual(str(out.parsetree), str(expected))
+			self.assertEqual(out.parsetree, expected)
+
 	def testToKnownParsesWhitespace(self):
 		pass
 
