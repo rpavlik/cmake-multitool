591841a643712b34a72c343bb4bc0b3735c80454
Ryan Pavlik <rpavlik@iastate.edu>
2010-01-07 09:43:53 -0600

    Start work on splitting out the cmake grammar

---
+++ cmakegrammar.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+"""
+CMake source file grammar
+
+Original Author:
+2010 Ryan Pavlik <rpavlik@iastate.edu> <abiryan@ryand.net>
+http://academic.cleardefinition.com
+Iowa State University HCI Graduate Program/VRAC
+"""
+
+import re
+
+
+def parse_line(line):
+	if line is not None:
+		m = reFullLine.match(line)
+		func, args, comment, fullLine = m.group("FuncName",
+											"Args", # todo change to argsCareful
+											"Comment",
+											"FullLine")
+
+		hasFullLine = (fullLine is not None)
+		if func is None:
+			func = ""
+
+		# TODO this is a suboptimal workaround!
+		if args == "":
+			args = None
+
+	return (func, args, comment, hasFullLine)
+
+####
+## Strings of sub-regexes to compile later - all are re.VERBOSE
+
+## all the functions that permit parens in their args
+_reParenArgFuncs = (r"(?ix)" # case-insensitive and verbose
+	+ "(?P<parenArgFunc>"
+	+ "|".join(
+		"""
+		if
+		else
+		elseif
+		endif
+
+		while
+		endwhile
+
+		math
+		""".split())
+	+ r")")
+
+## A possible function name
+_reFuncName = r"(?x) \s* (?P<FuncName> [\w\d]+) \s*"
+
+## Extremely general "non-empty arguments," no leading or trailing whitespc
+_reArgs = r"(?x) \s* (?P<Args> (\S ((\s)*\S)*)?) \s*"
+
+## Standard command args: no parens
+#_reArgsStd = r"(?x) \s* (?P<ArgsStd> ([\S-\(\)]([\S-\(\)]|\s[\S-\(\)])* )?) \s*"
+
+## A comment: everything after # as long as it's not preceded by a \
+# the ?<! is a "negative backward assertion handling "not after a \"
+# (?<!\\)
+_reComment = r"(?x) \s* (?P<Comment> (?<!\\)\#.*)"
+
+## The start of a command, up until the arguments
+_reCommandStart = _reFuncName + r"\("
+
+## The end of a command
+_reCommandEnd = r"\s*\)"
+
+## A full (complete) line
+_reFullLine = ( r"^(?P<FullLine>\s*"	# start the full line bool group
+			+ r"("			# start optional func call group
+			+ _reCommandStart
+			+ _reArgs
+			+ _reCommandEnd
+			+ r")?"			# end optional func call group
+			+ r"("			# start optional comment group
+			+ _reComment
+			+ r")?" 		# end optional comment group
+			+ r")?")		# end the full line bool group
+##
+####
+
+## Regex matching all the functions that permit parens in their args
+#reParenArgFuncs = re.compile("^" + _parenArgFuncs + "$",
+#	re.IGNORECASE | re.VERBOSE)
+
+## Regex matching a full line
+reFullLine = re.compile(_reFullLine, re.IGNORECASE | re.VERBOSE)
+
+## All functions that start a block
+_blockBeginnings = """	foreach
+						function
+						if
+						elseif
+						else
+						macro
+						while	""".split()
+_reBlockBeginnings = (r"(?ix)" +		# case-insensitive and verbose
+						r"(?P<BlockBeginnings>" +
+						"|".join(_blockBeginnings) +
+						r")")
+
+## A compiled regex that matches exactly the functions that start a block
+reBlockBeginnings = re.compile(_reBlockBeginnings, re.IGNORECASE)
+
+## A dictionary where blockEndings[blockBeginFunc]=(blockEndFunc1,...),
+## all datatypes are strings.  Size is one more than blockBeginnings
+## because we match the empty string opening function (dummy for start
+## of file) with a dummy EOF string as an end tag
+_blockEndings = {'' : ('_CMAKE_PARSER_EOF_FLAG_',),
+
+				'foreach'	: ('endforeach',),
+
+				'function'	: ('endfunction',),
+
+				'if'		: ('elseif', 'else', 'endif'),
+				'elseif'	: ('elseif', 'else', 'endif'),
+				'else'		: ('endif',),
+
+				'macro'	: ('endmacro',),
+
+				'while'	: ('endwhile',)}
+
+# Catch any "developer failed to add new function to both lists" bugs
+assert len(_blockEndings) - 1 == len(_blockBeginnings)
+
+## A big list comprehension that makes a dictionary with pairs (key, end)
+## where key is a start tag key from blockEndings and end is a
+## compiled regex that only exactly matches any corresponding ending
+dReBlockEndings = dict([  # Make a dict from list comprehension of pairs
+	# the dictionary key is just the start tag
+	( beginning,
+	# the value is a compiled regex matching any of the end tags
+	re.compile(	r"(?ix)" + # case insensitive
+				r"^(?P<BlockEnding>" +
+				r"|".join(ends) +
+				r")$", re.IGNORECASE) )
+
+	# for every key-value pair in the non-compiled dictionary
+	for beginning, ends in _blockEndings.iteritems() ])
+# end of huge list comprehension
+
+# sanity check the comprehension above
+assert len(_blockEndings) == len(dReBlockEndings)
+
+#if __name__ == "__main__":
+#	pass
---
+++ cmakegrammar_test.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python
+"""
+Tests for parsing CMake source files
+
+Original Author:
+2010 Ryan Pavlik <rpavlik@iastate.edu> <abiryan@ryand.net>
+http://academic.cleardefinition.com
+Iowa State University HCI Graduate Program/VRAC
+"""
+
+import unittest
+import re
+
+import cmakegrammar
+
+## Requirement:
+## All regexes must be able to be compiled
+class AllRegexesCompilable(unittest.TestCase):
+	def testCompileRegexes(self):
+		"""Compiling regex string attributes of the grammar"""
+		reKeys = 0
+		regexStrings = filter(lambda (x, y): re.match("(_re)", x),
+				cmakegrammar.__dict__.iteritems())
+		for (varName, reStr) in regexStrings:
+			re.compile(reStr)
+
+
+## Requirement:
+## Accept possible command names
+class REAcceptCommandNames(unittest.TestCase):
+	data = (	"func",
+				"FuNc",
+				"FUNC",
+				"the_func",
+				"the_1_FUNC"	)
+
+
+	subtest = ""
+	def _exc_info(self):
+		print "Subtest info:"
+		print self.subtest
+		return unittest.TestCase._exc_info(self)
+
+	def testAcceptValidCommandNames(self):
+		"""test the command name regex with just command names"""
+		for line in self.data:
+			self.subtest = line
+			self.assertTrue(re.match(cmakegrammar._reFuncName + "$", line))
+
+
+
+## Requirement:
+## Be able to parse a valid cmake command input line.
+class ParseCompleteLine(unittest.TestCase):
+	commandsOnly = (	("",				("", None, None)),
+					("func()",			("func", None, None)),
+					("func(arg)",		("func", "arg", None)),
+					("func(arg arg)",	("func", "arg arg", None)),
+					("func( arg arg )",	("func", "arg arg", None)),
+					("func( arg  ar )",	("func", "arg  ar", None)),
+					(r"func(\#notcmnt)",("func", r"\#notcmnt", None))	)
+
+	commentsOnly = (	"#",
+				"# comment",
+				"#comment",
+				"## comment",
+				"##comment",
+				"#a#a#a#"	)
+
+	mixed = (	("func() # cmnt",	("func", None, "# cmnt")),
+				(r"func(\#notcmnt) #iscmnt",("func", r"\#notcmnt", "#iscmnt"))	)
+
+	subtest = ""
+	def _exc_info(self):
+		print "Subtest info:"
+		print self.subtest
+		return unittest.TestCase._exc_info(self)
+
+	def testParseLineCommand(self):
+		"""parse_line on a line with a command only"""
+		for (line, expected) in self.commandsOnly:
+			self.subtest = line
+			func, args, comment, hasFullLine = cmakegrammar.parse_line(line)
+
+			self.assertTrue(hasFullLine)
+
+			self.assertEqual((func, args, comment), expected)
+
+	def testParseLineComment(self):
+		"""parse_line on a line with a comment only"""
+		for commentstring in self.commentsOnly:
+			self.subtest = commentstring
+			func, args, comment, hasFullLine = cmakegrammar.parse_line(commentstring)
+
+			self.assertTrue(hasFullLine)
+
+			self.assertEqual((func, args, comment), ("", None, commentstring))
+
+	def testParseLineMixed(self):
+		"""parse_line on a line with both a command and a comment"""
+		for (line, expected) in self.mixed:
+			self.subtest = line
+			func, args, comment, hasFullLine = cmakegrammar.parse_line(line)
+
+			self.assertTrue(hasFullLine)
+
+			self.assertEqual((func, args, comment), expected)
+
+## Requirement:
+## Notify on an incomplete cmake input line
+class ParsePartialLine(unittest.TestCase):
+	data = (	"func(",
+				"func(arg",
+				"func(arg ",
+				"func ( arg ",
+				"func(arg arg"	)
+
+
+	subtest = ""
+	def _exc_info(self):
+		print "Subtest info:"
+		print self.subtest
+		return unittest.TestCase._exc_info(self)
+
+	def testParseIncompleteStatement(self):
+		"""parse_line on an incomplete line with a command only"""
+		for line in self.data:
+			self.subtest = line
+			self.assertRaises(cmakegrammar.IncompleteStatementError,
+							  cmakegrammar.parse_line(line))
+
+
+### Requirement:
+### A given source file/string has only valid one parse
+#class KnownValues(unittest.TestCase):
+#	def setUp(self):
+#		import glob
+#		cmakes = glob.glob('./testdata/KnownValues/*.cmake')
+#		cmakes.sort()
+#		parses = glob.glob('./testdata/KnownValues/*.parse')
+#		parses.sort()
+#
+#		assert len(parses) == len(cmakes)
+#
+#		self.strings = []
+#		self.files = []
+#		self.uppers = []
+#		self.lowers = []
+#
+#		for cmakefn, parsefn in zip(cmakes, parses):
+#			cmakef = open(cmakefn, 'r')
+#			cmakestr = cmakef.read()
+#			cmakef.close()
+#
+#			parsef = open(parsefn, 'r')
+#			parsestr = parsef.read()
+#			parsef.close()
+#
+#			parsedata = eval(parsestr)
+#			parseupper = eval("None".join([x.upper() for x in parsestr.split("None")]))
+#			parselower = eval("None".join([x.lower() for x in parsestr.split("None")]))
+#
+#			self.strings.append( (cmakestr, parsedata) )
+#			self.files.append( (cmakefn, parsedata) )
+#			self.uppers.append( (cmakestr.upper(), parseupper) )
+#			self.lowers.append( (cmakestr.lower(), parselower) )
+#
+#	subtest = ""
+#	def _exc_info(self):
+#		print "Subtest info:"
+#		print self.subtest
+#		return unittest.TestCase._exc_info(self)
+#
+#	def testFullParseKnownString(self):
+#		"""passing in a known-good string to the full parser"""
+#		for instring, expected in self.strings:
+#			self.subtest = instring
+#			out = cmakeparser.parse_string(instring)
+#			self.assertEqual(out.parsetree, expected)
+#
+#	def testFullParseKnownFile(self):
+#		"""passing in a known-good input filename to the full parser"""
+#		for cmakefn, expected in self.files:
+#			self.subtest = cmakefn
+#			out = cmakeparser.parse_file(cmakefn)
+#			self.assertEqual(out.parsetree, expected)
+#
+#	def testFullParseKnownUppercaseString(self):
+#		"""passing in a known-good uppercased string to the full parser"""
+#		for instring, expected in self.uppers:
+#			self.subtest = instring
+#			out = cmakeparser.parse_string(instring)
+#			self.assertEqual(out.parsetree, expected)
+#
+#	def testFullParseKnownLowercaseString(self):
+#		"""passing in a known-good lowercased string to the full parser"""
+#		for instring, expected in self.lowers:
+#			self.subtest = instring
+#			out = cmakeparser.parse_string(instring)
+#			self.assertEqual(out.parsetree, expected)
+
+	## TODO
+	#def testToKnownParsesWhitespace(self):
+	#	pass
+
+## Requirement:
+## Parsing invalid source trees should fail
+# TODO
+
+if __name__=="__main__":
+	## Run tests if executed directly
+	try:
+		import nose
+		nose.main()
+	except (ImportError):
+		unittest.main()
