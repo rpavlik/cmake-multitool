f37663eec5c5edfba28b607966547028ec91e38f
Ryan Pavlik <rpavlik@iastate.edu>
2010-01-07 16:49:09 -0600

    Make the tests work, and move the output formatter out of the parser

--- cmakescript/cmakeparser.py
+++ cmakescript/cmakeparser.py
@@ -18,7 +18,9 @@ import re
 
 ###
 # internal packages
-# - none
+import cmakegrammar
+
+grammar = cmakegrammar
 
 def parse_string(instr):
 	parser = CMakeParser(ParseInput(instr))
@@ -78,43 +80,6 @@ class CMakeParser():
 		self.input = parseinput
 		self.parsetree = None
 
-	def output_as_cmake(self):
-		return "\n".join(self.output_block(self.parsetree, 0))
-
-	def output_block(self, block, level):
-		lines = []
-		if block is None:
-			return lines
-		for statement in block:
-			lines.extend(self.output_statement(statement, level))
-		return lines
-
-	def output_statement(self, statement, level):
-
-		func, args, comment, children = statement
-
-		if args is None:
-			args = ""
-
-		if func=="" and comment is None:
-			thisline = ""
-		else:
-			thisline = "\t" * level
-
-		if func!="":
-			thisline = thisline + func.lower() + "(" + args + ")"
-			if comment is not None:
-				thisline = thisline + "\t"
-
-		if comment is not None:
-			thisline = thisline + comment
-
-		output = [thisline]
-		# Recurse into children
-		output.extend(self.output_block(children, level + 1))
-		return output
-
-
 	def parse(self):
 		self.parsetree = self.parse_block_children(None)
 		if self.parsetree is None:
@@ -124,9 +89,9 @@ class CMakeParser():
 		if startTag is None:
 			# the block is the entire file.
 			isEnder = lambda x: (x is None)
-		elif self.reBlockBeginnings.match(startTag):
+		elif grammar.reBlockBeginnings.match(startTag):
 			# can have children
-			endblock = self.dReBlockEndings[startTag.lower()]
+			endblock = grammar.dReBlockEndings[startTag.lower()]
 			isEnder = endblock.match
 		else:
 			# Can have no children
@@ -134,7 +99,8 @@ class CMakeParser():
 
 		block = []
 		for line in self.input:
-			func, args, comment, complete = self.parse_line(line)
+			# TODO try-except IncompleteStatementError here
+			func, args, comment = grammar.parse_line(line)
 			assert complete
 			if isEnder(func):
 				print block
@@ -146,141 +112,6 @@ class CMakeParser():
 			block.append( statement )
 
 
-
-	def parse_line(self, line):
-		if line is not None:
-			m = self.reFullLine.match(line)
-			func, args, comment, fullLine = m.group("FuncName",
-												"Args", # todo change to argsCareful
-												"Comment",
-												"FullLine")
-
-			hasFullLine = (fullLine is not None)
-			if func is None:
-				func = ""
-
-			# TODO this is a suboptimal workaround!
-			if args == "":
-				args = None
-
-		return (func, args, comment, hasFullLine)
-
-	####
-	## Strings of sub-regexes to compile later - all are re.VERBOSE
-
-	## all the functions that permit parens in their args
-	_reParenArgFuncs = (r"(?ix)" # case-insensitive and verbose
-		+ "(?P<parenArgFunc>"
-		+ "|".join(
-			"""
-			if
-			else
-			elseif
-			endif
-
-			while
-			endwhile
-
-			math
-			""".split())
-		+ r")")
-
-	## A possible function name
-	_reFuncName = r"(?x) \s* (?P<FuncName> [\w\d]+) \s*"
-
-	## Extremely general "non-empty arguments," no leading or trailing whitespc
-	_reArgs = r"(?x) \s* (?P<Args> (\S ((\s)*\S)*)?) \s*"
-
-	## Standard command args: no parens
-	#_reArgsStd = r"(?x) \s* (?P<ArgsStd> ([\S-\(\)]([\S-\(\)]|\s[\S-\(\)])* )?) \s*"
-
-	## A comment: everything after # as long as it's not preceded by a \
-	# the ?<! is a "negative backward assertion handling "not after a \"
-	# (?<!\\)
-	_reComment = r"(?x) \s* (?P<Comment> (?<!\\)\#.*)"
-
-	## The start of a command, up until the arguments
-	_reCommandStart = _reFuncName + r"\("
-
-	## The end of a command
-	_reCommandEnd = r"\s*\)"
-
-	## A full (complete) line
-	_reFullLine = ( r"^(?P<FullLine>\s*"	# start the full line bool group
-				+ r"("			# start optional func call group
-				+ _reCommandStart
-				+ _reArgs
-				+ _reCommandEnd
-				+ r")?"			# end optional func call group
-				+ r"("			# start optional comment group
-				+ _reComment
-				+ r")?" 		# end optional comment group
-				+ r")?")		# end the full line bool group
-	##
-	####
-
-	## Regex matching all the functions that permit parens in their args
-	#reParenArgFuncs = re.compile("^" + _parenArgFuncs + "$",
-	#	re.IGNORECASE | re.VERBOSE)
-
-	## Regex matching a full line
-	reFullLine = re.compile(_reFullLine, re.IGNORECASE | re.VERBOSE)
-
-	## All functions that start a block
-	_blockBeginnings = """	foreach
-							function
-							if
-							elseif
-							else
-							macro
-							while	""".split()
-	_reBlockBeginnings = (r"(?ix)" +		# case-insensitive and verbose
-							r"(?P<BlockBeginnings>" +
-							"|".join(_blockBeginnings) +
-							r")")
-
-	## A compiled regex that matches exactly the functions that start a block
-	reBlockBeginnings = re.compile(_reBlockBeginnings, re.IGNORECASE)
-
-	## A dictionary where blockEndings[blockBeginFunc]=(blockEndFunc1,...),
-	## all datatypes are strings.  Size is one more than blockBeginnings
-	## because we match the empty string opening function (dummy for start
-	## of file) with a dummy EOF string as an end tag
-	_blockEndings = {'' : ('_CMAKE_PARSER_EOF_FLAG_',),
-
-					'foreach'	: ('endforeach',),
-
-					'function'	: ('endfunction',),
-
-					'if'		: ('elseif', 'else', 'endif'),
-					'elseif'	: ('elseif', 'else', 'endif'),
-					'else'		: ('endif',),
-
-					'macro'	: ('endmacro',),
-
-					'while'	: ('endwhile',)}
-
-	# Catch any "developer failed to add new function to both lists" bugs
-	assert len(_blockEndings) - 1 == len(_blockBeginnings)
-
-	## A big list comprehension that makes a dictionary with pairs (key, end)
-	## where key is a start tag key from blockEndings and end is a
-	## compiled regex that only exactly matches any corresponding ending
-	dReBlockEndings = dict([  # Make a dict from list comprehension of pairs
-		# the dictionary key is just the start tag
-		( beginning,
-		# the value is a compiled regex matching any of the end tags
-		re.compile(	r"(?ix)" + # case insensitive
-					r"^(?P<BlockEnding>" +
-					r"|".join(ends) +
-					r")$", re.IGNORECASE) )
-
-		# for every key-value pair in the non-compiled dictionary
-		for beginning, ends in _blockEndings.iteritems() ])
-	# end of huge list comprehension
-
-	# sanity check the comprehension above
-	assert len(_blockEndings) == len(dReBlockEndings)
 
 #if __name__ == "__main__":
 #	pass
