e0597847cea7b44a91244d88d3de6e80926f5805
Ryan Pavlik <rpavlik@iastate.edu>
2010-01-09 22:09:02 -0600

    Use a module-level fixture

--- cmakescript/cmakeparser_test.py
+++ cmakescript/cmakeparser_test.py
@@ -13,6 +13,7 @@ Iowa State University HCI Graduate Program/VRAC
 import unittest
 import re
 import os
+import glob
 
 ###
 # third-party packages
@@ -22,44 +23,79 @@ import os
 # internal packages
 import cmakeparser
 
+# format for each:
+# key is the filename - extension
+# value is whatever the variable name suggests
+inputfiles = dict()
+inputstrings = dict()
+inputuppers = dict()
+inputlowers = dict()
+
+parsedfiles = dict()
+parsedstrings = dict()
+
+parseduppers = dict()
+parsedlowers = dict()
+
+def setUp():
+	cmakes = glob.glob(os.path.split(__file__)[0] + '/testdata/KnownValues/*.cmake')
+	cmakes.sort()
+	parses = glob.glob(os.path.split(__file__)[0] + '/testdata/KnownValues/*.parse')
+	parses.sort()
+
+	assert len(parses) == len(cmakes)
+	assert len(parses) == 10
+
+
+
+	for cmakefn, parsefn in zip(cmakes, parses):
+		cbase = os.path.splitext(cmakefn)[0]
+		pbase = os.path.splitext(parsefn)[0]
+		assert cbase == pbase
+
+		cmakef = open(cmakefn, 'r')
+		cmakestr = cmakef.read()
+		cmakef.close()
+
+		parsef = open(parsefn, 'r')
+		parsestr = parsef.read()
+		parsef.close()
+
+		parsedata = eval(parsestr)
+
+		# case-change our known parses, but don't touch "None" because it's
+		#
+		applyExceptToNone = lambda userfunc, string, untouchable:	untouchable.join(
+			[	userfunc(chunk)
+				for chunk
+				in string.split(untouchable)	]	)
+
+		safeUpper = lambda input: applyExceptToNone(lambda x: x.upper(), input, "None")
+		safeLower = lambda input: applyExceptToNone(lambda x: x.lower(), input, "None")
+
+		parseupper = safeUpper(parsestr)
+		parselower = safeLower(parsestr)
+
+		inputfiles[cbase] = cmakefn
+		inputstrings[cbase] = cmakestr
+		inputuppers[cbase] = safeUpper(cmakestr)
+		inputlowers[cbase] = safeLower(cmakestr)
+
+		parsedfiles[cbase] = parsedata
+		parsedstrings[cbase] = parsedata
+
+		parseduppers[cbase] = parseupper
+		parsedlowers[cbase] = parselower
+
+	dataKeys = inputfiles.keys()
+
+
 ## Requirement:
 ## A given source file/string has only valid one parse
 class KnownValues(unittest.TestCase):
-	def setUp(self):
-		import glob
-		cmakes = glob.glob(os.path.split(__file__)[0] + '/testdata/KnownValues/*.cmake')
-		cmakes.sort()
-		parses = glob.glob(os.path.split(__file__)[0] + '/testdata/KnownValues/*.parse')
-		parses.sort()
-
-		assert len(parses) == len(cmakes)
-		assert len(parses) == 10
-
-		self.strings = []
-		self.files = []
-		self.uppers = []
-		self.lowers = []
-
-		for cmakefn, parsefn in zip(cmakes, parses):
-			cmakef = open(cmakefn, 'r')
-			cmakestr = cmakef.read()
-			cmakef.close()
-
-			parsef = open(parsefn, 'r')
-			parsestr = parsef.read()
-			parsef.close()
-
-			parsedata = eval(parsestr)
-			parseupper = eval("None".join([x.upper() for x in parsestr.split("None")]))
-			parselower = eval("None".join([x.lower() for x in parsestr.split("None")]))
-
-			self.strings.append( (cmakestr, parsedata) )
-			self.files.append( (cmakefn, parsedata) )
-			self.uppers.append( (cmakestr.upper(), parseupper) )
-			self.lowers.append( (cmakestr.lower(), parselower) )
-
 
 	subtest = ""
+
 	if "nose" in dir():
 		def _exc_info(self):
 			print "Subtest info:"
@@ -68,28 +104,36 @@ class KnownValues(unittest.TestCase):
 
 	def testFullParseKnownString(self):
 		"""passing in a known-good string to the full parser"""
-		for instring, expected in self.strings:
+		for key in parsedstrings.keys():
+			instring = inputstrings[key]
+			expected = parsedstrings[key]
 			self.subtest = instring
 			out = cmakeparser.parse_string(instring)
 			self.assertEqual(out.parsetree, expected)
 
 	def testFullParseKnownFile(self):
 		"""passing in a known-good input filename to the full parser"""
-		for cmakefn, expected in self.files:
-			self.subtest = cmakefn
+		for key in inputfiles.keys():
+			cmakefn = inputfiles[key]
+			expected = parsedfiles[key]
+			self.subtest = key
 			out = cmakeparser.parse_file(cmakefn)
 			self.assertEqual(out.parsetree, expected)
 
 	def testFullParseKnownUppercaseString(self):
 		"""passing in a known-good uppercased string to the full parser"""
-		for instring, expected in self.uppers:
+		for key in inputuppers.keys():
+			instring = inputuppers[key]
+			expected = parseduppers[key]
 			self.subtest = instring
 			out = cmakeparser.parse_string(instring)
 			self.assertEqual(out.parsetree, expected)
 
 	def testFullParseKnownLowercaseString(self):
 		"""passing in a known-good lowercased string to the full parser"""
-		for instring, expected in self.lowers:
+		for key in inputlowers.keys():
+			instring = inputlowers[key]
+			expected = parsedlowers[key]
 			self.subtest = instring
 			out = cmakeparser.parse_string(instring)
 			self.assertEqual(out.parsetree, expected)
